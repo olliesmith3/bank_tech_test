eecbde3748128640fb184e2bd6832c2a345ee891
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-02 13:05:11 +0000

    Refactored withdraw test for account class as not to have negative money

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,7 +10,7 @@ describe Account do
 
   describe '#withdraw' do 
     it 'Decreases the account balance by the deposit amount' do
-      my_account = Account.new('Mary Smith', 0.00)
+      my_account = Account.new('Mary Smith', 200.00)
       expect{ my_account.withdraw(100) }.to change { my_account.balance }.by(-100)
     end
   end

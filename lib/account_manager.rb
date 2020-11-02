202d12bf46b4bfa9fe0e2ef611fddf89635ed09f
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-02 12:38:10 +0000

    Minor - corrected name of create_account test

--- lib/account_manager.rb
+++ lib/account_manager.rb
@@ -1,6 +1,5 @@
 class AccountManager
   def self.create_account(holder_name, account_class = Account)
     account = account_class.new(holder_name)
-
   end
 end
--- spec/account_manager_spec.rb
+++ spec/account_manager_spec.rb
@@ -1,7 +1,7 @@
 require 'account_manager'
 
 describe AccountManager do
-  describe '#create' do 
+  describe '.create_account' do 
     it 'Creates a new account for Mary Smith with a balance of 0.00' do
       account_double = double(:account, :balance => 0.00, :holder_name => 'Mary Smith')
       account_class_double = double(:account_class, new: account_double)

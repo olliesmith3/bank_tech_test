ada1a9fd1fb688bf318b39779db0080f16c4cd86
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-02 12:36:44 +0000

    Added create_account test
    The test mocks the account class and an account instance and tests the account manager create_account method is returning an account instance.

---
+++ lib/account_manager.rb
@@ -0,0 +1,6 @@
+class AccountManager
+  def self.create_account(holder_name, account_class = Account)
+    account = account_class.new(holder_name)
+
+  end
+end
--- spec/account_manager_spec.rb
+++ spec/account_manager_spec.rb
@@ -1,1 +1,14 @@
 require 'account_manager'
+
+describe AccountManager do
+  describe '#create' do 
+    it 'Creates a new account for Mary Smith with a balance of 0.00' do
+      account_double = double(:account, :balance => 0.00, :holder_name => 'Mary Smith')
+      account_class_double = double(:account_class, new: account_double)
+
+      my_account = AccountManager.create_account('Mary Smith', account_class_double)
+      expect(my_account.balance).to eq 0.00
+      expect(my_account.holder_name).to eq 'Mary Smith'
+    end
+  end
+end

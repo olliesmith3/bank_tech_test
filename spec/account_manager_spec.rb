afd3469d73b659df293a571aff49f14b91501b0a
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-02 14:21:29 +0000

    Refactored test suite to remove repeat variable assignment

--- spec/account_manager_spec.rb
+++ spec/account_manager_spec.rb
@@ -1,11 +1,12 @@
 require 'account_manager'
 
 describe AccountManager do
+
+  let( :account_double       ) { double(:account)                            }
+  let( :account_class_double ) { double(:account_class, new: account_double) }
+
   describe '.create_account' do 
     it 'Creates a new account for Mary Smith with a balance of 0.00' do
-      account_double = double(:account)
-      account_class_double = double(:account_class, new: account_double)
-
       expect(AccountManager.create_account('Mary Smith', 0.00, account_class_double)).to eq account_double
     end
   end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,17 @@
 require 'account'
 
 describe Account do
+
+  let( :my_account               ) { Account.new('Mary Smith', 0.00)                     }
+  let( :transaction_double       ) { double(:transaction)                                }
+  let( :transaction_class_double ) { double(:transaction_class, new: transaction_double) }
+
   describe '#deposit' do 
     it 'Increases the account balance by the deposit amount' do
-      my_account = Account.new('Mary Smith', 0.00)
       expect{ my_account.deposit(100) }.to change { my_account.balance }.by(100)
     end
 
     it 'Adds a transaction object to the history array' do
-      transaction_double = double(:transaction)
-      transaction_class_double = double(:transaction_class, new: transaction_double)
-
-      my_account = Account.new('Mary Smith', 0.00)
       my_account.deposit(100, transaction_class_double)
       expect(my_account.history).to eq [transaction_double]
     end
@@ -24,8 +24,6 @@ describe Account do
     end
 
     it 'Raises an error if attempting to withdraw more money than the balance' do
-      my_account = Account.new('Mary Smith', 50.00)
-      expect { raise StandardError, 'this message exactly'}.to raise_error('this message exactly')
       expect { my_account.withdraw(100) }.to raise_error('Insufficient funds')
     end
   end

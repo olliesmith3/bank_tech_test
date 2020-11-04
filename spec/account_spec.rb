69c6489a97ab021f6bf38d83008b83957f9fe6c0
olliesmith3 <olliesmith3@rocketmail.com>
2020-11-04 11:12:06 +0000

    Added unit test for print_statement method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,7 +3,7 @@
 require 'account'
 
 describe Account do
-  let(:transaction_double) { double(:transaction) }
+  let(:transaction_double) { double(:transaction, amount: 100, date: '01/01/1994') }
   let(:transaction_class_double) { double(:transaction_class, new: transaction_double) }
   let(:my_account) { Account.new(0.00, transaction_class_double) }
 
@@ -27,7 +27,7 @@ describe Account do
   end
 
   describe '#withdraw' do
-    it 'Adds a tranmsaction to the history' do
+    it 'Adds a transaction to the history' do
       my_account = Account.new(200.00)
       expect { my_account.deposit(100) }.to change { my_account.history.length }.by(1)
     end
@@ -38,6 +38,13 @@ describe Account do
 
     it 'Raises an error if the amount provided is zero' do
       expect { my_account.withdraw(0) }.to raise_error('Amount must be a positive integer or decimal')
+    end
+  end
+
+  describe '#print_statement' do
+    it 'prints the history in the specified format' do
+      3.times { my_account.deposit(100) }
+      expect { my_account.print_statement }.to output("date || credit || debit || balance\n01/01/1994 || 100.00 || || 300.00\n01/01/1994 || 100.00 || || 200.00\n01/01/1994 || 100.00 || || 100.00\n").to_stdout
     end
   end
 end
